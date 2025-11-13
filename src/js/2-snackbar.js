58a0f7e871645058235e3f5f0b8da1c6310becf2
Andrey Abrosenkov <abrosenkov91@gmail.com>
2025-11-13 22:19:09 +0100

    test commit

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,4 @@
-import iziToast from 'izitoast';
+
 import 'izitoast/dist/css/iziToast.min.css';
 
 const formData = document.querySelector('.form');
@@ -25,20 +25,10 @@ function getPromise(e) {
 
   promise
     .then(res => {
-      iziToast.success({
-        icon: false,
-        message: `✅ Fulfilled promise in ${res}ms`,
-        position: 'topRight',
-        progressBar: false,
-      });
+      
     })
     .catch(error => {
-      iziToast.error({
-        icon: false,
-        message: `❌ Rejected promise in ${error}ms`,
-        position: 'topRight',
-        progressBar: false,
-      });
+      
     });
   formData.reset();
 }
